79996fcd32be48c1297abdbdecd4b4d2cba97a91
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-02-18 15:00:17 -0400

    auth-user

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -27,9 +27,55 @@ const perfil =  (req, res) => {
   res.json( {msg : 'mostrando perfil'});
 }
 
+const confirmar = async (req, res) => {
+  
+  const { token } = req.params;
+  const usuarioConfirmar = await Veterinario.findOne({token:token});
+  if(!usuarioConfirmar){
+    const error = new Error('Token no valido');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    usuarioConfirmar.token = null;
+    usuarioConfirmar.confirmed = true;
+    await usuarioConfirmar.save();
+    res.json( {msg: 'Usuario confirmado correctamente' });
+  } catch (error) {
+    console.log(error)
+  } 
+
+};
+
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+  const usuario = await Veterinario.findOne({email});
+  if(!usuario){
+    const error = new Error('El usuario no existe');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  //Confirmar si el usuario esta autenticado
+  if(!usuario.confirmed){
+    const error = new Error('Tu cuenta no ha sido confirmada');
+    return res.status(403).json({msg: error.message});
+  }
+
+  //Autenticar al usuario
+  //Revisar el password
+  if(await usuario.comprobarPassword(password)){
+    console.log('password correcto');
+  } else {
+    const error = new Error('El password es incorrecto');
+    return res.status(403).json({msg: error.message});
+  }
+};
+
 export {
   registrar,
-  perfil
+  perfil,
+  confirmar,
+  autenticar
 };
 
 
