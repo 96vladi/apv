66420548c60a400e27dff3c9ba88d19b72a6fce0
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-02-19 16:40:54 -0400

    adding-to-the-vet-in-patients

--- backend/routes/PacienteRoutes.js
+++ backend/routes/PacienteRoutes.js
@@ -3,11 +3,12 @@ import {
   agregarPaciente,
   obtenerPaciente 
 } from '../controllers/pacienteController.js';
+import checkAuth from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 router.route('/')
-  .post(agregarPaciente)
+  .post(checkAuth, agregarPaciente)
   .get(obtenerPaciente);
 
 export default router;
