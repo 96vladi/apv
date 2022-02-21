96cb3b7713d55d41c2cfc2060994a6eebbd431fd
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-02-21 13:54:11 -0400

    login-screen

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom'
 export const AuthLayout = () => {
   return (
     <>
-      <h1>Desde AuthLayout</h1>
-
-      <Outlet />
+      <main className='container mx-auto md:grid md:grid-cols-2 mt-12 p-2'>
+        <Outlet />
+      </main>
     </>
   )
 }
