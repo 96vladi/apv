aa43bccc9e9111a28eaf1445c2c74596fb7b9700
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-02-23 13:52:53 -0400

    add-mailtrap-to-verify-email

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"; 
 import dotenv from 'dotenv';
+import cors from 'cors';
 import conectarDb from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js';
 import pacienteRoutes from './routes/PacienteRoutes.js';
@@ -11,6 +12,21 @@ app.use(express.json());
 dotenv.config();
 
 conectarDb();
+
+const dominiosPermitidos = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(dominiosPermitidos.indexOf(origin) !== -1){
+      //EL origen del request esta permitido
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
 app.use('/api/veterinarios', veterinarioRoutes);
 
 app.use('/api/pacientes', pacienteRoutes);
