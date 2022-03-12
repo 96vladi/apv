a69cfdbd572cd5a468aa939fdd06d1d8f35730c7
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2022-03-11 23:08:35 -0400

    creating-protected-routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthLayout } from './layout/AuthLayout';
+import { RutaProtegida } from './layout/RutaProtegida';
+
 import { Login } from './paginas/Login';
 import { Registrar } from './paginas/Registrar';
 import { OlvidePassword } from './paginas/OlvidePassword';
 import { ConfirmarCuenta } from './paginas/ConfirmarCuenta';
 import { NuevoPassword } from './paginas/NuevoPassword';
+
 import { AuthProvider } from './context/AuthProvider';
+import AdministrarPacientes from './paginas/AdministrarPacientes';
 
 
 function App() {
@@ -14,12 +18,18 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          {/* Area publica */}
           <Route path='/' element={<AuthLayout />}>
             <Route index element={<Login />}/>
             <Route path='registrar' element={<Registrar />}/>
             <Route path='olvide-password' element={<OlvidePassword />}/>
             <Route path='olvide-password/:token' element={<NuevoPassword />}/>
             <Route path='confirmar/:id' element={<ConfirmarCuenta />}/>
+          </Route>
+
+          {/* Area protegida */}
+          <Route path='/admin' element={<RutaProtegida />}>
+            <Route index element={<AdministrarPacientes />}/>
           </Route>
         </Routes>
       </AuthProvider>
---
+++ frontend/src/layout/RutaProtegida.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export const RutaProtegida = () => {
+  return (
+    <>
+      <h1>Esta es una ruta protegida</h1>
+      <Outlet />
+    </>
+  )
+}
